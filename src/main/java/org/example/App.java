ba6302f51e406f494fea17793936ce13399d9137
Minardo Toirac Isla <gensparkweeks@gmail.com>
2022-08-02 21:26:41 -0400

    adding classes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,16 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Student student = (Student) context.getBean("student");
+
+        System.out.println(student);
     }
 }
---
+++ src/main/java/org/example/Phone.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Phone {
+
+    private String mob;
+
+    //Constructors
+    public Phone() {
+        this.mob = "502-101-0001";
+    }
+
+    public Phone(String mob) {
+        this.mob = mob;
+    }
+
+    //Setter
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    //Getter
+    public String getMob() {
+        return mob;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "mob='" + mob + '\'' +
+                '}';
+    }
+}
